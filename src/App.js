0e7aaba211d0c5f1137e18f9896e1230e1800b43
Edi Kurniawan <edikurniawan.dev@gmail.com>
2022-06-22 17:07:40 +0700

    major update todo list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,120 @@
+import React, { useState } from 'react';
+import TodoInput from './components/TodoInput';
+import TodoItem from './components/TodoItem';
+
 export default function App() {
-    return <div className="card"></div>;
+    if (!localStorage.getItem('todoItems')) {
+        localStorage.setItem('todoItems', JSON.stringify([]));
+    }
+
+    const [todoItems, setTodoItems] = useState(
+        JSON.parse(localStorage.getItem('todoItems')),
+    );
+    const [openModal, setOpenModal] = useState(false);
+    const [itemTodo, setItemTodo] = useState({});
+    const [index, setIndex] = useState();
+
+    const createTodoItem = (todo) => {
+        const newTodoItems = [...todoItems, { todo, complete: false }];
+        setTodoItems(newTodoItems);
+        localStorage.setItem('todoItems', JSON.stringify(newTodoItems));
+    };
+
+    const deleteTodoItem = (index) => {
+        const newTodoItems = [...todoItems];
+        newTodoItems.splice(index, 1);
+        setTodoItems(newTodoItems);
+        localStorage.setItem('todoItems', JSON.stringify(newTodoItems));
+    };
+
+    const completeTodoItem = (index) => {
+        const newTodoItems = [...todoItems];
+        newTodoItems[index].complete === false
+            ? (newTodoItems[index].complete = true)
+            : (newTodoItems[index].complete = false);
+        setTodoItems(newTodoItems);
+        localStorage.setItem('todoItems', JSON.stringify(newTodoItems));
+    };
+
+    const openModalDialog = (index) => {
+        const newTodoItems = [...todoItems];
+        const item = newTodoItems[index];
+        setItemTodo(item);
+        setIndex(index);
+        setOpenModal(!openModal);
+    };
+
+    const updateTodoItem = (todo, index) => {
+        const newTodoItems = [...todoItems];
+        const item = newTodoItems[index];
+        let todoObj = { todo, complete: item.complete };
+        newTodoItems.splice(index, 1, todoObj);
+        setTodoItems(newTodoItems);
+        setOpenModal(false);
+        localStorage.setItem('todoItems', JSON.stringify(newTodoItems));
+    };
+
+    return (
+        <div className="px-3">
+            <div className="max-w-4xl px-10 pb-20 mx-auto mt-10 border shadow-lg card bg-base-100">
+                <h1 className="mt-5 mb-10 text-2xl font-bold text-center text-gray-700 capitalize ">
+                    write it✍️ and do it💪
+                </h1>
+                <TodoInput createTodoItem={createTodoItem} todo="" />
+                <ul className="mt-10 space-y-2">
+                    {todoItems.map((item, index) => (
+                        <TodoItem
+                            key={index}
+                            index={index}
+                            item={item}
+                            deleteTodoItem={deleteTodoItem}
+                            completeTodoItem={completeTodoItem}
+                            openModalDialog={openModalDialog}
+                        />
+                    ))}
+                </ul>
+
+                {/* modal */}
+                <input
+                    type="checkbox"
+                    id="my-modal-3"
+                    className="modal-toggle"
+                    checked={openModal}
+                    readOnly
+                />
+                <div className="items-start modal">
+                    <div className="relative top-20 modal-box">
+                        <label
+                            onClick={() => {
+                                setOpenModal(false);
+                            }}
+                            htmlFor="my-modal-3"
+                            className="absolute text-white btn btn-error btn-sm btn-circle right-2 top-2">
+                            ✕
+                        </label>
+                        <h3 className="mb-5 text-lg font-bold">Update Task</h3>
+                        <TodoInput
+                            updateTodoItem={updateTodoItem}
+                            todo={itemTodo.todo ?? ''}
+                            index={index}
+                        />
+                    </div>
+                </div>
+            </div>
+            <p className="mt-5 text-center text-gray-500">
+                Created with{' '}
+                <a
+                    href="https://tailwindcss.com/"
+                    className="font-medium text-sky-600 hover:underline">
+                    TailwindCSS
+                </a>{' '}
+                and{' '}
+                <a
+                    href="https://daisyui.com/"
+                    className="font-medium text-amber-400 hover:underline">
+                    DaisyUI
+                </a>
+            </p>
+        </div>
+    );
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,62 @@
+export default function TodoItem({
+    item,
+    index,
+    completeTodoItem,
+    openModalDialog,
+    deleteTodoItem,
+}) {
+    // console.log(index);
+    return (
+        <div className="">
+            <li className="flex items-start justify-between px-4 py-3 md:flex-row rounded-xl bg-slate-100">
+                <div className="flex items-start">
+                    <input
+                        type="checkbox"
+                        onClick={() => completeTodoItem(index)}
+                        className="mt-1 mr-5 checkbox checkbox-accent"
+                        checked={item.complete}
+                        readOnly
+                    />
+                    <p
+                        className={
+                            'font-bold text-lg whitespace-wrap ' +
+                            (item.complete ? 'text-gray-400' : '')
+                        }>
+                        {item.todo}
+                    </p>
+                </div>
+                <div className="flex items-center h-full space-x-2">
+                    <button
+                        onClick={() => openModalDialog(index)}
+                        disabled={item.complete}
+                        className="text-white btn btn-circle btn-sm btn-warning">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="w-5 h-5"
+                            viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                        </svg>
+                    </button>
+                    <button
+                        onClick={() => deleteTodoItem(index)}
+                        className="text-white btn btn-circle btn-sm btn-error">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="w-6 h-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M6 18L18 6M6 6l12 12"
+                            />
+                        </svg>
+                    </button>
+                </div>
+            </li>
+        </div>
+    );
+}
